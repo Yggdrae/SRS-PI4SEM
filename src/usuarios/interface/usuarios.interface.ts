5e70b9d137e68612e40c4cd4b649986f151da398
RenanMMz <renan.m.mlol@gmail.com>
2025-04-28 10:41:48 -0300

    UsuarioInterface ajustada

--- src/usuarios/interface/usuarios.interface.ts
+++ src/usuarios/interface/usuarios.interface.ts
@@ -1,6 +1,8 @@
-export class createUserDTO {
+export interface UsuarioInterface {
+    id: number;
     nome: string;
     email: string;
     senha: string;
+    criadoEm: Date;
     tipo: string;
 }
